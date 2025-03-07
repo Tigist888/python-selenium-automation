ca5daa9a7a4c733f01fd317916cd6d409819e5c6
Tigist D <tigist2274@gmail.com>
2025-03-07 15:07:11 -0600

    update code

--- target_search_script.py
+++ target_search_script.py
@@ -13,24 +13,33 @@ service = Service(driver_path)
 driver = webdriver.Chrome(service=service)
 driver.maximize_window()
 
+
+# get the path to the ChromeDriver executable
+driver_path = ChromeDriverManager().install()
+
+# create a new Chrome browser instance
+service = Service(driver_path)
+driver = webdriver.Chrome(service=service)
+driver.maximize_window()
+
 # open the url
 driver.get('https://www.target.com/')
 
 driver.find_element(By.ID, 'search').send_keys('tea')
+driver.find_element(By.XPATH, "//button[@data-test='@web/Search/SearchButton']").click()
+sleep(6)
 
-driver.find_element(By.XPATH, "//button[@data-test='@web/search/searchButton']").click()
-sleep(4)
-
-#verification:
-#by finding 1 element
-# driver.find_element(By.XPATH,  "//div[@data-test='lp-resultsCount']")
-# print('test case passed')
+# verification:
+# by finding 1 element
+# driver.find_element(By.XPATH, "//div[@data-test='lp-resultsCount']")
+# print('Test case passed')
 
 # by checking text
-actual_text=driver.find_element(By.XPATH,  "//div[@data-test='lp-resultsCount']").text
-print('actual text:\n', actual_text)
+actual_text = driver.find_element(By.XPATH, "//div[@data-test='lp-resultsCount']").text
+expected_text = 'tea'
 
-assert 'tea' in actual_text, f'Error. Text tea not in {actual_text}'
-print('test case passed')
+assert expected_text in actual_text, f'Error. Text {expected_text} not in {actual_text}'
+print('Test case passed')
 
-sleep(10)
+# Note: please do not use if/else for verification, you test case must fail with an Exception if a feature is broken
+# sleep(10)
