12e8eee72d27773f18d28775e64d3581160a8cba
Tigist D <tigist2274@gmail.com>
2025-03-22 12:44:15 -0500

    update code lesson 5

--- features/steps/Target_sign_in.py
+++ features/steps/Target_sign_in.py
@@ -1,37 +1,37 @@
-from selenium.webdriver.common.by import By
-from behave import given, when, then
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from time import sleep
-from target_search_script import expected_text
-
-
-@given("open target main page")
-def open_target_main(context):
-    context.driver.get('https://www.target.com/')
-
-
+# from selenium.webdriver.common.by import By
+# from behave import given, when, then
+# from selenium.webdriver.support.wait import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
+# from time import sleep
+# from target_search_script import expected_text
 #
-@when('click sign in')
-def click_sign_in(context):
-    context.driver.find_element(By.CSS_SELECTOR, '.sc-43f80224-3.fBDEOp.h-margin-r-x3' ).click()
-    #sleep(6)
-    context.driver.implicitly_wait(5)
-
-
-@when('From right side navigation menu,click sign in')
-def right_side_click_sign_in(context):
-
-    wait=WebDriverWait(context.driver, 10).until(
-        EC.element_to_be_clickable((By.CSS_SELECTOR, "[data-test='accountNav-signIn']"))
-    )
-    wait.click()
-    #context.driver.find_element(By.CSS_SELECTOR, "[data-test='accountNav-signIn']").click()
-    #sleep(6)
-
-
-@then('verify sign in form opened')
-def verify_search_results(context):
-    expected_result='to be opened'
-    actual_text = context.driver.find_element(By.XPATH, "//span[text()='Sign into your Target account']")
-    assert expected_result in actual_text, f'Error. Text {expected_text} not in {actual_text}'
+#
+# @given("open target main page")
+# def open_target_main(context):
+#     context.driver.get('https://www.target.com/')
+#
+#
+# #
+# @when('click sign in')
+# def click_sign_in(context):
+#     context.driver.find_element(By.CSS_SELECTOR, '.sc-43f80224-3.fBDEOp.h-margin-r-x3' ).click()
+#     #sleep(6)
+#     context.driver.implicitly_wait(5)
+#
+#
+# @when('From right side navigation menu,click sign in')
+# def right_side_click_sign_in(context):
+#
+#     wait=WebDriverWait(context.driver, 10).until(
+#         EC.element_to_be_clickable((By.CSS_SELECTOR, "[data-test='accountNav-signIn']"))
+#     )
+#     wait.click()
+#     #context.driver.find_element(By.CSS_SELECTOR, "[data-test='accountNav-signIn']").click()
+#     #sleep(6)
+#
+#
+# @then('verify sign in form opened')
+# def verify_search_results(context):
+#     expected_result='to be opened'
+#     actual_text = context.driver.find_element(By.XPATH, "//span[text()='Sign into your Target account']")
+#     assert expected_result in actual_text, f'Error. Text {expected_text} not in {actual_text}'
--- features/steps/loop_product_clors.py
+++ features/steps/loop_product_clors.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from behave import given, then
-from time import sleep
 
 
 
@@ -12,15 +13,17 @@ def open_product_page(context):
 
 @then('verify user can click through colors')
 def verify_color_selection(context):
+    wait = WebDriverWait(context.driver, 10)
     # Find all color options
-    color_options = context.driver.find_elements(By.XPATH, "//li[@class='styles_ndsCarouselItem__dnUkr']")
+    color_options = wait.until(
+        EC.presence_of_all_elements_located((By.XPATH, "//li[contains(@class, 'ndsCarouselItem')]")))
 
     for color in color_options:
         color.click()
-        sleep(1)
 
         # Verify selected color is displayed
-        selected_color = context.driver.find_element(By.CSS_SELECTOR, "[data-test='@web/VariationComponent'] div")
+        selected_color = wait.until(
+            EC.visibility_of_element_located((By.CSS_SELECTOR, "[data-test='@web/VariationComponent'] div")))
         assert selected_color.is_displayed(), "Selected color is not displayed correctly"
 
     print("All colors were clicked and verified successfully.")
